3393fff76b86c46550348813a7468dd75bc72b3d
Keith Simmons <ksimmons@gmail.com>
2013-06-07 14:29:15 -0400

    auto formatting for src to improve readability

--- networkTcpClientExample/src/testApp.cpp
+++ networkTcpClientExample/src/testApp.cpp
@@ -13,7 +13,7 @@ int portNum = 21211;
 string ipString = "127.0.0.1";
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void testApp::setup() {
 
     // xml connection settings
     ofxXmlSettings					connectionXML;
@@ -30,37 +30,37 @@ void testApp::setup(){
     // done loading connection xml!
 
 
-	// we don't want to be running too fast
-	ofSetVerticalSync(true);
+    // we don't want to be running too fast
+    ofSetVerticalSync(true);
 
-	//some variables:
-	//have we typed
-	//typed	= false;
+    //some variables:
+    //have we typed
+    //typed	= false;
 
-	//our typing position
-	pos		= 0;
+    //our typing position
+    pos		= 0;
 
-	//our send and recieve strings
-	msgTx	= "";
-	msgRx	= "";
+    //our send and recieve strings
+    msgTx	= "";
+    msgRx	= "";
 
-	//are we connected to the server - if this fails we
-	//will check every few seconds to see if the server exists
+    //are we connected to the server - if this fails we
+    //will check every few seconds to see if the server exists
 
-	weConnected = tcpClient.setup(ipString, portNum); // server
+    weConnected = tcpClient.setup(ipString, portNum); // server
 
-	//optionally set the delimiter to something else.  The delimter in the client and the server have to be the same
-	tcpClient.setMessageDelimiter("\n");
+    //optionally set the delimiter to something else.  The delimter in the client and the server have to be the same
+    tcpClient.setMessageDelimiter("\n");
 
-	connectTime = 0;
-	deltaTime = 0;
+    connectTime = 0;
+    deltaTime = 0;
 
-	tcpClient.setVerbose(true);
+    tcpClient.setVerbose(true);
 
     gui.addTitle("Send messages");
-	gui.addButton("Send RESET", sendResetButton);
-	gui.addButton("Send RECORD", sendNewVisitorButton);
-	gui.addButton("Send STATUS", sendStatusButton);
+    gui.addButton("Send RESET", sendResetButton);
+    gui.addButton("Send RECORD", sendNewVisitorButton);
+    gui.addButton("Send STATUS", sendStatusButton);
     gui.addButton("Send SET INACTIVITY", sendSetInactivityButton);
 
     gui.addTitle("Sensors").setNewColumn(true);
@@ -68,47 +68,47 @@ void testApp::setup(){
     gui.addSlider("Weight in pounds", visitorWeight, 1.0, 1000.0);
 
 
-	gui.loadFromXML();
+    gui.loadFromXML();
 
     gui.setAlignRight(true);
 
-	gui.show();
+    gui.show();
 
     ofEnableAlphaBlending();
-	HHLlogo.loadImage("HHL_logo.png");
+    HHLlogo.loadImage("HHL_logo.png");
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
+void testApp::update() {
 
     if(sendResetButton) {
-		sendResetButton = false;
+        sendResetButton = false;
         msgTx = "RESET";
     }
 
     if(sendNewVisitorButton) {
-		sendNewVisitorButton = false;
+        sendNewVisitorButton = false;
 
-		string randomVisitorID = ofToString(int(ofRandom(0,99999999)));
+        string randomVisitorID = ofToString(int(ofRandom(0,99999999)));
 
         msgTx = "RECORD: " + randomVisitorID + ", " + ofToString(visitorHeight) + ", " + ofToString(visitorWeight);
     }
 
     if(sendStatusButton) {
-		sendStatusButton = false;
+        sendStatusButton = false;
         msgTx = "STATUS";
     }
 
     if(sendSetInactivityButton) {
-		sendSetInactivityButton = false;
+        sendSetInactivityButton = false;
         msgTx = "SET INACTIVITY 60";
     }
 
-	ofBackground(230, 227, 230);
+    ofBackground(230, 227, 230);
 
-	//we are connected - lets send our text if delimeter present and check what we get back
-	if(weConnected){
-		if( msgTx.length() > 0 && tcpClient.send(msgTx)){ // ONLY SEND IF MESSAGE LENGTH > 0
+    //we are connected - lets send our text if delimeter present and check what we get back
+    if(weConnected) {
+        if( msgTx.length() > 0 && tcpClient.send(msgTx)) { // ONLY SEND IF MESSAGE LENGTH > 0
             cout << "message sent to server, clearing" << endl;
 
             msgTx = "";
@@ -119,58 +119,58 @@ void testApp::update(){
 //				msgRx = str;
 //                cout << "updated msgRx from server: " << msgRx << endl;
 //			}
-		}else if(!tcpClient.isConnected()){
-			weConnected = false;
-		}
+        } else if(!tcpClient.isConnected()) {
+            weConnected = false;
+        }
 
-		//we're connected, lets update our text
+        //we're connected, lets update our text
         string str = tcpClient.receive(); // ALWAYS RECEIVE IF CONNECTED
-        if( str.length() > 0 ){
+        if( str.length() > 0 ) {
             msgRx = str;
             cout << "latest msgRx from server: " << msgRx << endl;
         }
-	}else{
-		//if we are not connected lets try and reconnect every 5 seconds
-		deltaTime = ofGetElapsedTimeMillis() - connectTime;
+    } else {
+        //if we are not connected lets try and reconnect every 5 seconds
+        deltaTime = ofGetElapsedTimeMillis() - connectTime;
 
-		if( deltaTime > 5000 ){
-			weConnected = tcpClient.setup(ipString, portNum);
-			connectTime = ofGetElapsedTimeMillis();
-		}
+        if( deltaTime > 5000 ) {
+            weConnected = tcpClient.setup(ipString, portNum);
+            connectTime = ofGetElapsedTimeMillis();
+        }
 
-	}
+    }
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void testApp::draw() {
 
     ofSetColor(255);
     HHLlogo.draw( ofGetWindowWidth() - HHLlogo.getWidth() - 20, ofGetWindowHeight() - HHLlogo.getHeight() - 20 );
 
-	ofSetColor(20, 20, 20);
-	ofDrawBitmapString("openFrameworks TCP Send Example", 15, 30+101);
+    ofSetColor(20, 20, 20);
+    ofDrawBitmapString("openFrameworks TCP Send Example", 15, 30+101);
 
 //	if(typed){
 //		ofDrawBitmapString("sending:", 15, 55+101);
 //		ofDrawBitmapString(msgTx, 85, 55+101);
 //	}
 //	else{
-		if(weConnected)
-            ofDrawBitmapString("status: click something to send data to \nip: " + ipString + " port: " + ofToString( portNum ), 15, 55+101);
-		else
-            ofDrawBitmapString("status: server not found. launch server app and check ports!\n\nreconnecting in "+ofToString( (5000 - deltaTime) / 1000 )+" seconds", 15, 55+101);
+    if(weConnected)
+        ofDrawBitmapString("status: click something to send data to \nip: " + ipString + " port: " + ofToString( portNum ), 15, 55+101);
+    else
+        ofDrawBitmapString("status: server not found. launch server app and check ports!\n\nreconnecting in "+ofToString( (5000 - deltaTime) / 1000 )+" seconds", 15, 55+101);
 //	}
 
-	ofDrawBitmapString("from server: \n"+msgRx, 15, 130+101);
+    ofDrawBitmapString("from server: \n"+msgRx, 15, 130+101);
 
-	gui.draw();
+    gui.draw();
 }
 
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
+void testApp::keyPressed(int key) {
 
-	//you can only type if you're connected
+    //you can only type if you're connected
 //	if(weConnected){
 //		if(key == 13)key = '\n'; // CR
 //		if(key == 8 || key == 127){ // BS or DEL
@@ -186,42 +186,42 @@ void testApp::keyPressed(int key){
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
+void testApp::keyReleased(int key) {
 
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void testApp::mouseMoved(int x, int y ) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
+void testApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void testApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
+void testApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
+void testApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
+void testApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){
+void testApp::dragEvent(ofDragInfo dragInfo) {
 
 }
